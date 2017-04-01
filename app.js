7ec4a1ddb09583b7853431da8b4539dad123c9e1
Kuba Matyka <kmatyka@ntrc.eu>
2017-04-01 16:47:04 +0200

    clear input data after submit

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-// Looping with ngRepeat
-
 var myApp = angular.module('myApp', []);
 
 myApp.controller('myAppController', ['$scope', function myAppController($scope) {
   $scope.names = ['Asia', 'Kasia', 'Zosia'];
 
   $scope.addName = function () {
-    $scope.names.push($scope.inputName)
+    $scope.names.push($scope.inputName);
+    $scope.inputName = '';
   }
 }]);
