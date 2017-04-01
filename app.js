1829289083689c38ab69c3621b7782c15794c07d
Kuba Matyka <kmatyka@ntrc.eu>
2017-04-01 16:54:24 +0200

    use ngClick directive to delete data entry

--- app.js
+++ app.js
@@ -7,4 +7,9 @@ myApp.controller('myAppController', ['$scope', function myAppController($scope) 
     $scope.names.push($scope.inputName);
     $scope.inputName = '';
   }
+  
+  $scope.removeName = function (nameText) {
+    var i = $scope.names.indexOf(nameText);
+    $scope.names.splice(i, 1);
+  }
 }]);
