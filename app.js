89a6a537c926b48447ef60a43b27632a66957cd7
Kuba Matyka <kmatyka@ntrc.eu>
2017-04-02 11:44:00 +0200

    dynamically change sorting

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+'use strict';
+
 var myApp = angular.module('myApp', []);
 
 myApp.controller('myAppCtrl', ['$scope', '$http', function myAppCtrl($scope, $http) {
   $http.get('countries.json').success(function (data) {
       $scope.countries = data;
-    })
+    });
+
+  $scope.sortField = 'population';
+  $scope.reverse = true;
+  $scope.sortFields = function (data) {
+    $scope.sortField = data;
+    $scope.reverse = !$scope.reverse;
+  }
 }]);
