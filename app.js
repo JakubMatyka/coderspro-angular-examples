cebad57e3f3b4d8379c527fe9678ba898d17c0fe
Kuba Matyka <kmatyka@ntrc.eu>
2017-04-02 12:47:42 +0200

    change structure of project

--- app.js
+++ app.js
@@ -6,11 +6,4 @@ myApp.controller('myAppCtrl', ['$scope', '$http', function myAppCtrl($scope, $ht
   $http.get('countries.json').success(function (data) {
       $scope.countries = data;
     });
-
-  $scope.sortField = 'population';
-  $scope.reverse = true;
-  $scope.sortFields = function (data) {
-    $scope.sortField = data;
-    $scope.reverse = !$scope.reverse;
-  }
 }]);
