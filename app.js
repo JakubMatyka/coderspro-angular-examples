25477d43f2ab84ed03bf587210a52bed4c9b69f1
Kuba Matyka <kmatyka@ntrc.eu>
2017-04-02 09:36:00 +0200

    loop through array of obj

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('myAppController', ['$scope', function myAppController($scope) {
-  $scope.names = ['Asia', 'Kasia', 'Zosia'];
-
-  $scope.addName = function () {
-    $scope.names.push($scope.inputName);
-    $scope.inputName = '';
-  }
-  
-  $scope.removeName = function (nameText) {
-    var i = $scope.names.indexOf(nameText);
-    $scope.names.splice(i, 1);
-  }
+  $scope.countries = [
+    { name: 'China', population: 1300000000 },
+    { name: 'India', population: 1982364762 },
+    { name: 'United States', population: 248927549 }
+  ]
 }]);
