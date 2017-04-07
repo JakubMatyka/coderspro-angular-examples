72527fe7ce183a9cb188f8587561c8b76357654c
Kuba Matyka <kmatyka@ntrc.eu>
2017-04-08 01:15:29 +0200

    Create a simple directive for a single element in the list

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+// Create a custom directive
+
 'use strict';
 
 var countryApp = angular.module('countryApp', [
   'ngRoute'
 ]);
 
-countryApp.config(['$routeProvider', function($routeProvider) {
+countryApp.config(['$routeProvider', function ($routeProvider) {
   $routeProvider
     .when('/', {
       templateUrl: 'country-list.html',
@@ -19,7 +21,7 @@ countryApp.config(['$routeProvider', function($routeProvider) {
     })
 }]);
 
-countryApp.factory('countries', ['$http', function($http) {
+countryApp.factory('countries', ['$http', function ($http) {
   return {
     list: function (callback) {
       $http({
@@ -31,25 +33,47 @@ countryApp.factory('countries', ['$http', function($http) {
     find: function (id, callback) {
       $http({
         method: 'GET',
-        url: "country_" + id + ".json",
+        url: 'country_' + id + '.json',
         cache: true
       }).success(callback)
     }
   };
 }]);
-countryApp.controller('CountryListCtrl', ['$scope', 'countries',
-function($scope, countries) {
-  countries.list(function (countries) {
-    $scope.countries = countries;
-  })
-}]);
-countryApp.controller('CountryDetailCtrl', ['$scope', '$routeParams', 'countries',
-function ($scope, $routeParams, countries) {
-  countries.find($routeParams.countryId, function(country) {
-    $scope.country = country;
-  });
 
-  $scope.goBack = function () {
-    window.history.back();
-  }
-}]);
+// Minimum config to make this work
+// We have a list of countries and each element of this list is executing some logic like fetching data from JSON file
+// for that particular country
+
+countryApp.directive('country', function(){
+  return {
+    // restrict to use it as an attribute
+    restrict: 'A',
+    // By declaring scope property we isolate the scope of our directive from other part of our code.
+    // A bi-directional binding can be set up between the local scope property and the parent property using the = symbol.
+    // If the parent model changes, just like in normal data-binding then the local property will reflect the change.
+    // What happens here is a country from parent in country list passes a data to nested element and binds them.
+    // And because of that in our template for directive we can refer to that variable.
+    scope: {
+      country: '='
+    },
+    templateUrl: 'country.html'
+  };
+});
+
+countryApp.controller('CountryListCtrl', ['$scope', 'countries',
+  function ($scope, countries) {
+    countries.list(function (countries) {
+      $scope.countries = countries;
+    })
+  }]);
+
+countryApp.controller('CountryDetailCtrl', ['$scope', '$routeParams', 'countries',
+  function ($scope, $routeParams, countries) {
+    countries.find($routeParams.countryId, function (country) {
+      $scope.country = country;
+    });
+
+    $scope.goBack = function () {
+      window.history.back();
+    }
+  }]);
